5affd88d4caa0594ad810bd46e348f3c72ac1e2d
SarahAndDuck <ratkevirina@gmail.com>
2023-06-19 15:06:14 +0700

    feat:add slider and smooth page navigation and smooth scrolling

--- script.js
+++ script.js
@@ -55,3 +55,123 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
     document.querySelector(href).scrollIntoView({ behavior: 'smooth' });
   }
 });
+
+///////////////////////////////////////
+////  слайдер
+///////////////////////////////////////
+
+const slides = document.querySelectorAll('.slide');
+// slides.forEach(
+//   (slide, index) => (slide.style.transform = `translateX(${index * 100}%)`)
+//   // 1 -0%
+//   // 2-100%
+//   // 3-200%
+//   // 3-300%
+// );
+let currentSlide = 0;
+const slidesNumber = slides.length - 1;
+const slider = document.querySelector('.slider');
+
+const moveToSlider = function () {};
+// /////////////////////////////////////////////////
+// временное отображение для разрабор=тки
+
+// slider.style.transform = 'scale(0.4)';
+// slider.style.overflow = 'visible';
+// /////////////////////////////////////////////////
+const btnLeft = document.querySelector('.slider__btn--left');
+const btnRight = document.querySelector('.slider__btn--right');
+
+// //////////////////////////////////////////
+// беcконечный слайдер
+//
+// Цикл по слайдам
+let sliders = [];
+for (let i = 0; i < slides.length; ++i) {
+  // Добавляем изображение из слайда
+  sliders[i] = slides[i].querySelector('img');
+  // Удаляем слайд
+  slides[i].remove();
+}
+let viewport = document.querySelector('.slider').offsetWidth;
+// Позиция сайда
+let offset = 0;
+// Функция для отрисовки сайда
+function drow() {
+  // Создаём элемент слайда
+  let slide = document.createElement('div');
+  // Добавляем касс slide новому элементу
+  slide.classList.add('slide');
+  // Добавляем дочерний элемент контент слайда
+  slide.appendChild(sliders[currentSlide]);
+  // Даём позицию для слайда
+  slide.style.left = offset * viewport + 'px';
+  // Добавляем слайд в контейнер слайдеров
+  document.querySelector('.slider').appendChild(slide);
+  // Если следующий слайд равен числу слайдеров, то
+  if (currentSlide + 1 == slides.length) {
+    // Включаем первый слайд
+    currentSlide = 0;
+  } else {
+    // Иначе
+    // Увеличиваем номер слайда на один
+    currentSlide++;
+  }
+  // Назначаем позицию слайда
+  offset = 1;
+}
+drow();
+drow();
+// Создаём функцию для переключения
+function left() {
+  // Запрещаем событие кнопки вперёд на клик
+  btnRight.removeEventListener('click', left);
+  // Берём слайды
+  let slides2 = document.querySelectorAll('.slide');
+  // Позиция
+  let offset2 = 0;
+  // Цикл для перемещения слайда
+  for (let i = 0; i < slides2.length; i++) {
+    // Меняем позицию слайда
+    slides2[i].style.left = offset2 * viewport - viewport + 'px';
+    // Увеличиваем позицию
+    offset2++;
+  }
+  // Ставим таймер
+  setTimeout(function () {
+    // Удаляем прошлый слайд
+    slides2[0].remove();
+    // Рисуем следующий слайд
+    drow();
+    // Назначаем событие на кнопку вперёд
+    btnRight.addEventListener('click', left);
+  }, 500);
+}
+
+function right() {
+  // Запрещаем событие кнопки вперёд на клик
+  btnLeft.removeEventListener('click', right);
+  // Берём слайды
+  let slides2 = document.querySelectorAll('.slide');
+  // Позиция
+  let offset2 = 0;
+  // Цикл для перемещения слайда
+  for (let i = 0; i < slides2.length; i++) {
+    // Меняем позицию слайда
+    slides2[i].style.left = offset2 * viewport - viewport + 'px';
+    // Увеличиваем позицию
+    offset2++;
+  }
+  // Ставим таймер
+  setTimeout(function () {
+    // Удаляем прошлый слайд
+    slides2[0].remove();
+    // Рисуем следующий слайд
+    drow();
+    // Назначаем событие на кнопку вперёд
+    btnLeft.addEventListener('click', right);
+  }, 500);
+}
+
+btnRight.addEventListener('click', left);
+btnLeft.addEventListener('click', right);
